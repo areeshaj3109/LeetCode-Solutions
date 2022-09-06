c776c62917570e17daf09183904a33714eb252e2
Areesha Jiwani <67435405+areeshaj3109@users.noreply.github.com>
2022-09-07 00:48:15 +0500

    Time: 559 ms (59.55%), Space: 0B (100.00%) - LeetHub

--- 1393-capital-gain-loss/1393-capital-gain-loss.sql
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -1,10 +1,9 @@
 # Write your MySQL query statement below
 with result as
 (select stock_name, 
-    sum(price) - lag(sum(price)) over (partition by stock_name order by stock_name) as capital_gain_loss
+    sum(price) - lag(sum(price)) over (partition by stock_name) as capital_gain_loss
 from stocks
-group by stock_name, operation
-order by stock_name)
+group by stock_name, operation)
 
 select stock_name, capital_gain_loss
 from result
