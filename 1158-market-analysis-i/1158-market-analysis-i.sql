88f82ca7f3850eef6b8c4b2c5718727d386dd4f8
Areesha Jiwani <67435405+areeshaj3109@users.noreply.github.com>
2022-09-07 01:49:49 +0500

    Time: 2812 ms (5.00%), Space: 0B (100.00%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,5 @@
+SELECT u.user_id AS buyer_id, u.join_date AS join_date  , 
+coalesce(count(buyer_id),0) AS orders_in_2019 
+FROM Users AS u
+LEFT JOIN Orders AS o ON u.user_id = o.buyer_id AND YEAR(o.order_date) = 2019
+GROUP BY u.user_id
