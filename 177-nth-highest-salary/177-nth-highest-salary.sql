88ab539f8594aa74adc04ae16f1712e851c21e6e
Areesha Jiwani <67435405+areeshaj3109@users.noreply.github.com>
2022-09-07 01:55:39 +0500

    Time: 392 ms (61.93%), Space: 0B (100.00%) - LeetHub

---
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -0,0 +1,16 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      
+      
+      with result as(
+      select *, dense_rank() over (order by salary desc) as getNthHighestSalary
+      from employee)
+      
+      select salary
+      from result
+      where getNthHighestSalary = N
+      limit 1
+      
+  );
+END
